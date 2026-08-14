38752d7202531d97d0929df24337dbe435699647
laien <laien.rain@gmail.com>
2026-08-14 14:23:52 +0800

    fix: 增加测试数据和方法

---
+++ examples/remesh.sh
@@ -0,0 +1,14 @@
+#conda env create -f env.yaml   # 如果 pamo 环境尚未创建
+#conda activate pamo
+
+python ./example.py --input ./examples/111.stl --output ./examples/111_remeshed.stl --remesh-only --remesh-resolution 256
+ # @FilePath: \pamo\examples\remesh.sh
+ # @Author: laien laien.rain@gmail.com
+ # @Date: 2026-07-30 18:16:50
+ # ----------------------------------------------
+ # @LastEditors: laien laien.rain@gmail.com
+ # @LastEditTime: 2026-07-30 18:18:48
+ # @Description: 
+ # ----------------------------------------------
+ # Copyright (c) 2026 Supreium Co., Ltd , All Rights Reserved.
+### 
